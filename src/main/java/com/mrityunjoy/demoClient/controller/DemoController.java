59618cbb632c0b3efbe257e937b9444833adf4ae
coderperfect <mrityunjoynath38@gmail.com>
2022-08-02 09:57:37 +0530

    Get token and send the same

---
+++ src/main/java/com/mrityunjoy/demoClient/controller/DemoController.java
@@ -0,0 +1,48 @@
+package com.mrityunjoy.demoClient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/demo")
+@Slf4j
+public class DemoController {
+	@Value("${HOST_URL:http://localhost:8080}")
+	private String hostUrl;
+	
+	@GetMapping("/weak-login")
+	public Mono<TokenResponse> loginWeak() {
+		String username = "user";
+		String password = "password";
+		
+		WebClient webClient = WebClient.builder()
+				.filter(ExchangeFilterFunctions
+						.basicAuthentication(username, password))
+				.build();
+		
+		return webClient.get().uri(hostUrl + "/login")
+				.exchangeToMono(res -> {
+			log.info("Sending token weak security");
+			return res.bodyToMono(TokenResponse.class);
+		});
+	}
+	
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Getter
+	@Setter
+	private static class TokenResponse {
+		private String token;
+	}
+}
